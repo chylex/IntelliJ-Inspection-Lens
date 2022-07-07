chylex/IntelliJ-Disable-Task-Contexts

Add build and plugin configuration

settings.gradle.kts
@@ -1,0 +1,1 @@
+rootProject.name = "DisableTaskContexts"

chylex/IntelliJ-Inspection-Lens

settings.gradle.kts
@@ -1,0 +1,1 @@
+rootProject.name = "InspectionLens"