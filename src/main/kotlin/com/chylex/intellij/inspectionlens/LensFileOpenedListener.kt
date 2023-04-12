86a3a87da2a2eea14e05d7e3deee4b15dd92d3ca
chylex <contact@chylex.com>
2023-04-12 06:59:52 +0200

    Replace deprecated FileEditorManagerListener API

--- src/main/kotlin/com/chylex/intellij/inspectionlens/LensFileOpenedListener.kt
+++ src/main/kotlin/com/chylex/intellij/inspectionlens/LensFileOpenedListener.kt
@@ -1,7 +1,7 @@
 package com.chylex.intellij.inspectionlens
 
 import com.intellij.openapi.fileEditor.FileEditorManager
-import com.intellij.openapi.fileEditor.FileEditorManagerListener
+import com.intellij.openapi.fileEditor.FileOpenedSyncListener
 import com.intellij.openapi.fileEditor.TextEditor
 import com.intellij.openapi.fileEditor.ex.FileEditorWithProvider
 import com.intellij.openapi.vfs.VirtualFile
@@ -9,8 +9,8 @@ import com.intellij.openapi.vfs.VirtualFile
 /**
  * Listens for newly opened editors, and installs a [LensMarkupModelListener] on them.
  */
-class LensFileEditorListener : FileEditorManagerListener {
-	override fun fileOpenedSync(source: FileEditorManager, file: VirtualFile, editorsWithProviders: MutableList<FileEditorWithProvider>) {
+class LensFileOpenedListener : FileOpenedSyncListener {
+	override fun fileOpenedSync(source: FileEditorManager, file: VirtualFile, editorsWithProviders: List<FileEditorWithProvider>) {
 		for (editorWrapper in editorsWithProviders) {
 			val fileEditor = editorWrapper.fileEditor
 			if (fileEditor is TextEditor) {
