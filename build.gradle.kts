a05a97274bd1a9e0637aaf53c29133e02ee11024
chylex <contact@chylex.com>
2023-11-05 17:57:24 +0100

    Update Gradle to 8.4 & update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	kotlin("jvm") version "1.8.0"
-	id("org.jetbrains.intellij") version "1.13.3"
+	id("org.jetbrains.intellij") version "1.16.0"
 }
 
 group = "com.chylex.intellij.inspectionlens"
@@ -14,19 +14,19 @@ repositories {
 	mavenCentral()
 }
 
-dependencies {
-	testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
+intellij {
+	version.set("2023.1")
+	updateSinceUntilBuild.set(false)
+	
+	plugins.add("tanvd.grazi")
 }
 
 kotlin {
 	jvmToolchain(17)
 }
 
-intellij {
-	version.set("2023.1")
-	updateSinceUntilBuild.set(false)
-	
-	plugins.add("tanvd.grazi")
+dependencies {
+	testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 }
 
 tasks.patchPluginXml {
