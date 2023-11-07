182ad049d91eab0809a8265a282bbc4aa66ddf55
chylex <contact@chylex.com>
2023-11-07 21:27:42 +0100

    Convert a plugin.xml service to a light service

--- src/main/kotlin/com/chylex/intellij/inspectionlens/InspectionLensPluginDisposableService.kt
+++ src/main/kotlin/com/chylex/intellij/inspectionlens/InspectionLensPluginDisposableService.kt
@@ -1,10 +1,12 @@
 package com.chylex.intellij.inspectionlens
 
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.components.Service
 
 /**
  * Gets automatically disposed when the plugin is unloaded.
  */
+@Service
 class InspectionLensPluginDisposableService : Disposable {
 	override fun dispose() {}
 }
